5874fa2bba419b295f245411ffe2d666ebfcf55b
Sarwar-Putul <sarwarputul@gmail.com>
2021-04-12 04:25:33 +0600

    Home page done

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import {
   Route,
 } from "react-router-dom";
 import './App.css';
+import Appointment from './Componants/Appointment/Appointment/Appointment';
 import Home from './Componants/Home/Home/Home';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Home></Home>
+        </Route>
+        <Route path="/appointment">
+          <Appointment></Appointment>
         </Route>
 
       </Switch>
